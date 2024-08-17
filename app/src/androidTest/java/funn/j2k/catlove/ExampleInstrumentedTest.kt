725a6cc023796e380546fb4bd2f146ec809f3615
jaka2005 <jakalogin1808@gmail.com>
2024-08-18 01:43:42 +0300

    initial commit

---
+++ app/src/androidTest/java/funn/j2k/catlove/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package funn.j2k.catlove
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("funn.j2k.catlove", appContext.packageName)
+    }
+}
